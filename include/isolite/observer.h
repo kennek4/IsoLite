7e38bf232243366435d50768f386d2c57ff3bab9
kennek4 <kentabanay@gmail.com>
2025-04-24 14:12:11 -0600

    feat: Added the Observer class to IsoLite namespace

--- include/isolite/observer.h
+++ include/isolite/observer.h
@@ -1,43 +1,14 @@
 #ifndef ISO_OBSERVER_H
 #define ISO_OBSERVER_H
 
-#include <algorithm>
-#include <vector>
+#include "events.h"
 
-typedef enum IsoEvent {
-
-} IsoEvent;
-
+namespace IsoLite {
 class Observer {
 public:
   virtual ~Observer() {};
   virtual void onNotify(const IsoEvent) = 0;
 };
-
-class Subject {
-public:
-  void addObserver(Observer *observer) {
-    int count = std::count(_observers.begin(), _observers.end(), observer);
-    if (count == 0) { // Observer doesn't already exist
-      _observers.emplace_back(observer);
-    };
-  };
-
-  void removeObserver(Observer *observer) {
-    auto itr = std::find(_observers.begin(), _observers.end(), observer);
-    if (itr != _observers.end()) { // Observer exists, we can delete pointer
-      _observers.erase(itr);
-    }
-  };
-
-  void notifyObservers(const IsoEvent event) {
-    for (int i = 0; i < _observers.size(); i++) {
-      _observers[i]->onNotify(event);
-    };
-  };
-
-private:
-  std::vector<Observer *> _observers;
-};
+}; // namespace IsoLite
 
 #endif // !ISO_OBSERVER_H
